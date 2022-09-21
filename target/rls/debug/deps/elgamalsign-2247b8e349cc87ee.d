f0546dfc9d0588830a86c5e5044f8104a4f2d230
Usman Shahid <26981965+usmanshahid86@users.noreply.github.com>
2022-09-21 17:18:46 +0500

    updated elgamal signature serialization

--- target/rls/debug/deps/elgamalsign-2247b8e349cc87ee.d
+++ target/rls/debug/deps/elgamalsign-2247b8e349cc87ee.d
@@ -1,9 +1,10 @@
-/Users/quasar/Work/ZkOS/target/rls/debug/deps/elgamalsign-2247b8e349cc87ee.rmeta: elgamalsign/src/lib.rs elgamalsign/src/errors.rs elgamalsign/src/key.rs elgamalsign/src/signature.rs elgamalsign/src/transcript.rs
+/Users/quasar/Work/ZkOS/target/rls/debug/deps/elgamalsign-2247b8e349cc87ee.rmeta: elgamalsign/src/lib.rs elgamalsign/src/errors.rs elgamalsign/src/key.rs elgamalsign/src/serialization.rs elgamalsign/src/signature.rs elgamalsign/src/transcript.rs
 
-/Users/quasar/Work/ZkOS/target/rls/debug/deps/elgamalsign-2247b8e349cc87ee.d: elgamalsign/src/lib.rs elgamalsign/src/errors.rs elgamalsign/src/key.rs elgamalsign/src/signature.rs elgamalsign/src/transcript.rs
+/Users/quasar/Work/ZkOS/target/rls/debug/deps/elgamalsign-2247b8e349cc87ee.d: elgamalsign/src/lib.rs elgamalsign/src/errors.rs elgamalsign/src/key.rs elgamalsign/src/serialization.rs elgamalsign/src/signature.rs elgamalsign/src/transcript.rs
 
 elgamalsign/src/lib.rs:
 elgamalsign/src/errors.rs:
 elgamalsign/src/key.rs:
+elgamalsign/src/serialization.rs:
 elgamalsign/src/signature.rs:
 elgamalsign/src/transcript.rs:
