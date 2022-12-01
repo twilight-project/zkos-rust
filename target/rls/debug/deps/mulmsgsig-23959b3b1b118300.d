5c2a6c85fd01e87162a2314b9a220bec21ebadf6
usmanshahid <usman.shahid@nu.edu.pk>
2022-12-01 11:35:20 +0500

    added TransactionTransfer support

--- target/rls/debug/deps/mulmsgsig-23959b3b1b118300.d
+++
@@ -1,9 +0,0 @@
-/Users/quasar/Work/ZkOS/target/rls/debug/deps/mulmsgsig-23959b3b1b118300.rmeta: mulmsgsig/src/lib.rs mulmsgsig/src/context.rs mulmsgsig/src/multisignature.rs mulmsgsig/src/errors.rs mulmsgsig/src/transcript.rs
-
-/Users/quasar/Work/ZkOS/target/rls/debug/deps/mulmsgsig-23959b3b1b118300.d: mulmsgsig/src/lib.rs mulmsgsig/src/context.rs mulmsgsig/src/multisignature.rs mulmsgsig/src/errors.rs mulmsgsig/src/transcript.rs
-
-mulmsgsig/src/lib.rs:
-mulmsgsig/src/context.rs:
-mulmsgsig/src/multisignature.rs:
-mulmsgsig/src/errors.rs:
-mulmsgsig/src/transcript.rs:
