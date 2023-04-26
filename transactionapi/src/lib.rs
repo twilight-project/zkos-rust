b09d3ba4f85e50f54fda7ff6c988c4a4e2424397
Siddharth Parashar <20308880+shanu516516@users.noreply.github.com>
2023-04-26 22:20:08 +0530

    rpc response

--- transactionapi/src/lib.rs
+++ transactionapi/src/lib.rs
@@ -2,3 +2,18 @@ pub mod rpcclient;
 pub mod rpcserver;
 #[macro_use]
 extern crate lazy_static;
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct TransactionStatusId {
+    pub txid: String,
+}
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct TxResponse {
+    pub response: String,
+}
+impl TxResponse {
+    pub fn new(response: String) -> Self {
+        TxResponse { response: response }
+    }
+}
