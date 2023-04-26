4c80ba6ee5c3368b01cc808207f9b8bbf2254fff
Siddharth Parashar <20308880+shanu516516@users.noreply.github.com>
2023-04-26 22:20:46 +0530

    main update

--- transactionapi/src/bin/api_server.rs
+++ transactionapi/src/bin/api_server.rs
@@ -10,44 +10,44 @@ use transaction::reference_tx::{
     create_dark_reference_transaction, create_qq_reference_transaction,
 };
 fn main() {
-    let handle = std::thread::Builder::new()
-        .name(String::from("rpc request"))
-        .spawn(move || {
-            std::thread::sleep(std::time::Duration::from_millis(5000));
-            let tx = create_qq_reference_transaction();
+    // let handle = std::thread::Builder::new()
+    //     .name(String::from("rpc request"))
+    //     .spawn(move || {
+    //         std::thread::sleep(std::time::Duration::from_millis(5000));
+    //         let tx = create_qq_reference_transaction();
 
-            let tx_send: RpcBody<Transaction> = RpcRequest::new(tx, Method::TxQueue);
-            let res = tx_send.send("http://127.0.0.1:3030".to_string());
+    //         let tx_send: RpcBody<Transaction> = RpcRequest::new(tx, Method::TxQueue);
+    //         let res = tx_send.send("http://127.0.0.1:3030".to_string());
 
-            // println!("res:{:#?}", res.unwrap().bytes());
+    //         // println!("res:{:#?}", res.unwrap().bytes());
 
-            // let resp: Resp = serde_json::from_slice(&res.unwrap().bytes().unwrap()).unwrap();
-            // println!("res:{:#?}", resp);
-            match res {
-                Ok(x) => {
-                    println!("res:{:#?}", x);
-                }
-                Err(arg) => {
-                    println!("errr:{:#?}", arg);
-                }
-            }
-            let tx_send: RpcBody<TransactionStatusId> = RpcRequest::new(
-                TransactionStatusId {
-                    txid: "5f516a8a-fc68-4a34-b299-373dcaae6b4c".to_string(),
-                },
-                Method::TxStatus,
-            );
-            let res = tx_send.send("http://127.0.0.1:3030".to_string());
-            match res {
-                Ok(x) => {
-                    println!("res:{:#?}", x);
-                }
-                Err(arg) => {
-                    println!("errr:{:#?}", arg);
-                }
-            }
-        })
-        .unwrap();
+    //         // let resp: Resp = serde_json::from_slice(&res.unwrap().bytes().unwrap()).unwrap();
+    //         // println!("res:{:#?}", resp);
+    //         match res {
+    //             Ok(x) => {
+    //                 println!("res:{:#?}", x);
+    //             }
+    //             Err(arg) => {
+    //                 println!("errr:{:#?}", arg);
+    //             }
+    //         }
+    //         let tx_send: RpcBody<TransactionStatusId> = RpcRequest::new(
+    //             TransactionStatusId {
+    //                 txid: "5f516a8a-fc68-4a34-b299-373dcaae6b4c".to_string(),
+    //             },
+    //             Method::TxStatus,
+    //         );
+    //         let res = tx_send.send("http://127.0.0.1:3030".to_string());
+    //         match res {
+    //             Ok(x) => {
+    //                 println!("res:{:#?}", x);
+    //             }
+    //             Err(arg) => {
+    //                 println!("errr:{:#?}", arg);
+    //             }
+    //         }
+    //     })
+    //     .unwrap();
     rpcserver();
-    handle.join().unwrap();
+    // handle.join().unwrap();
 }
