1fad81ccc0f91814f42274753cead8290cbc460b
Siddharth Parashar <20308880+shanu516516@users.noreply.github.com>
2023-05-17 12:24:50 +0530

    inputype based db

--- utxo-in-memory/src/db/types.rs
+++ utxo-in-memory/src/db/types.rs
@@ -1,1 +1,7 @@
 pub type LogSequence = usize;
+
+pub enum TxInputType {
+    Coin = 0,  //uint8
+    Memo = 1,  //uint8
+    State = 2, //uint8
+}
--- utxo-in-memory/src/main.rs
+++ utxo-in-memory/src/main.rs
@@ -1,5 +1,15 @@
+use std::collections::{HashMap, HashSet};
+use utxolib::db;
+use utxolib::db::init_utxo;
 #[macro_use]
 extern crate lazy_static;
 fn main() {
-    println!("hello wolrd");
+    // init utxo
+    init_utxo();
+    // db::leveldbrep::leveldbtest();
+    // let mut db: HashMap<i64, String> = HashMap::new();
+    // let x = db.insert(1, String::from("value 1"));
+    // let x = db.try_insert(1, String::from("value 2"));
+
+    // println!("db: {:#?}", db);
 }
