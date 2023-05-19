e1044d28eb754717a12057c039d5338c150fe7f4
Siddharth Parashar <20308880+shanu516516@users.noreply.github.com>
2023-05-19 16:44:07 +0530

    get snapshot struct and impl complete

--- utxo-in-memory/src/main.rs
+++ utxo-in-memory/src/main.rs
@@ -1,6 +1,22 @@
 // #[macro_use]
 extern crate lazy_static;
+
 fn main() {
     // init utxo
     // init_utxo();
+    let mut keyst = Testmut { key: 3 };
+    println!("first:{:#?}", keyst);
+    keyst.check();
+    println!("updated:{:#?}", keyst);
+}
+
+#[derive(Debug, Clone)]
+pub struct Testmut {
+    key: usize,
+}
+
+impl Testmut {
+    pub fn check(&mut self) {
+        self.key = 5;
+    }
 }
