4efd3f2b092369bc3a98d75ba3f841734226ccc5
Siddharth Parashar <20308880+shanu516516@users.noreply.github.com>
2023-06-09 03:02:56 +0530

    minor bug fix

---
+++ utxo-in-memory/src/blockoperations/mod.rs
@@ -0,0 +1,4 @@
+// mod utxodb_operations;
+// pub use self::utxodb_operations::*;
+mod initialset;
+pub use self::initialset::*;
--- utxo-in-memory/src/dbcurd/mod.rs
+++
@@ -1,4 +0,0 @@
-// mod utxodb_operations;
-// pub use self::utxodb_operations::*;
-mod initialset;
-pub use self::initialset::*;
