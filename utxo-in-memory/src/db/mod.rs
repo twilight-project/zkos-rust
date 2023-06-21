6755b0c8b73408afd9f3de79a804c3bef09ab4cb
Siddharth Parashar <20308880+shanu516516@users.noreply.github.com>
2023-06-22 03:40:53 +0530

    block proccessing complete

--- utxo-in-memory/src/db/mod.rs
+++ utxo-in-memory/src/db/mod.rs
@@ -7,6 +7,6 @@ mod types;
 
 pub use self::persistdb::SnapShot;
 pub use self::types::*;
-mod utxostore_tt;
-pub use self::utxostore_tt::LocalDBtrait;
-pub use self::utxostore_tt::LocalStorage;
+mod utxostore;
+pub use self::utxostore::LocalDBtrait;
+pub use self::utxostore::LocalStorage;
