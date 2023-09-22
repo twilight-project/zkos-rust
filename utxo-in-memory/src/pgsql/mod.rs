edb570c60eea7e41e83414fafbe63a974d6c72ec
Siddharth Parashar <20308880+shanu516516@users.noreply.github.com>
2023-09-22 12:03:43 +0530

    init sql in lib

--- utxo-in-memory/src/pgsql/mod.rs
+++ utxo-in-memory/src/pgsql/mod.rs
@@ -1,6 +1,6 @@
 mod initiate_sql;
 mod sql;
 mod test_tx;
-pub use self::initiate_sql::{POSTGRESQL_POOL_CONNECTION, THREADPOOL_SQL_QUEUE};
+pub use self::initiate_sql::{init_psql, POSTGRESQL_POOL_CONNECTION, THREADPOOL_SQL_QUEUE};
 pub use self::sql::*;
 pub use self::test_tx::{deserialize_tx_id, deserialize_tx_string, tx_id_string};
