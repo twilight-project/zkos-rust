a7731585b20498a8f72a3a4574aa463df7cb4e03
usmanshahid86 <usmanshahid86@gmail.com>
2023-09-26 01:01:29 +0500

    updated test

--- transactionapi/src/bin/api_server.rs
+++ transactionapi/src/bin/api_server.rs
@@ -13,44 +13,44 @@ use transaction::reference_tx::{
 };
 use utxo_in_memory::{init_utxo, zk_oracle_subscriber};
 fn main() {
-    let handle = std::thread::Builder::new()
-        .name(String::from("rpc request"))
-        .spawn(move || {
-            std::thread::sleep(std::time::Duration::from_millis(5000));
-            let tx = create_dark_reference_transaction();
+    // let handle = std::thread::Builder::new()
+    //     .name(String::from("rpc request"))
+    //     .spawn(move || {
+    //         std::thread::sleep(std::time::Duration::from_millis(5000));
+    //         let tx = create_dark_reference_transaction();
 
-            let tx_send: RpcBody<Transaction> = RpcRequest::new(tx, Method::TxCommit);
-            let res = tx_send.send("http://127.0.0.1:3030".to_string());
+    //         let tx_send: RpcBody<Transaction> = RpcRequest::new(tx, Method::TxCommit);
+    //         let res = tx_send.send("http://127.0.0.1:3030".to_string());
 
-            // println!("res:{:#?}", res.unwrap().bytes());
+    //         // println!("res:{:#?}", res.unwrap().bytes());
 
-            // let resp: Resp = serde_json::from_slice(&res.unwrap().bytes().unwrap()).unwrap();
-            // println!("res:{:#?}", resp);
-            match res {
-                Ok(x) => {
-                    println!("res1:{:#?}", x);
-                }
-                Err(arg) => {
-                    println!("errr1:{:#?}", arg);
-                }
-            }
-            let tx_send: RpcBody<TransactionStatusId> = RpcRequest::new(
-                TransactionStatusId {
-                    txid: "5f516a8a-fc68-4a34-b299-373dcaae6b4c".to_string(),
-                },
-                Method::TxStatus,
-            );
-            let res = tx_send.send("http://127.0.0.1:3030".to_string());
-            match res {
-                Ok(x) => {
-                    println!("res:{:#?}", x);
-                }
-                Err(arg) => {
-                    println!("errr:{:#?}", arg);
-                }
-            }
-        })
-        .unwrap();
+    //         // let resp: Resp = serde_json::from_slice(&res.unwrap().bytes().unwrap()).unwrap();
+    //         // println!("res:{:#?}", resp);
+    //         match res {
+    //             Ok(x) => {
+    //                 println!("res1:{:#?}", x);
+    //             }
+    //             Err(arg) => {
+    //                 println!("errr1:{:#?}", arg);
+    //             }
+    //         }
+    //         let tx_send: RpcBody<TransactionStatusId> = RpcRequest::new(
+    //             TransactionStatusId {
+    //                 txid: "5f516a8a-fc68-4a34-b299-373dcaae6b4c".to_string(),
+    //             },
+    //             Method::TxStatus,
+    //         );
+    //         let res = tx_send.send("http://127.0.0.1:3030".to_string());
+    //         match res {
+    //             Ok(x) => {
+    //                 println!("res:{:#?}", x);
+    //             }
+    //             Err(arg) => {
+    //                 println!("errr:{:#?}", arg);
+    //             }
+    //         }
+    //     })
+    //     .unwrap();
     init_utxo();
 
     let handle = thread::spawn(|| {
