e3960ca6ae9b5b0c44ad57d2005074b80de26291
usmanshahid86 <usmanshahid86@gmail.com>
2023-11-16 17:00:05 +0500

    added api endpoint for querying all state and memo utxo ids

--- utxo-in-memory/src/blockoperations/blockprocessing.rs
+++ utxo-in-memory/src/blockoperations/blockprocessing.rs
@@ -407,6 +407,46 @@ pub fn all_coin_type_utxo() -> Vec<String> {
     }
     return result;
 }
+pub fn all_memo_type_utxo() -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+    let mut utxo_storage = UTXO_STORAGE.lock().unwrap();
+    let input_type = IOType::Memo as usize;
+    let utxos = utxo_storage.data.get_mut(&input_type).unwrap();
+    for (key, output_data) in utxos {
+        match bincode::deserialize(&key) {
+            Ok(value) => {
+                let utxo: Utxo = value;
+                let hex_str: String = utxo.to_hex();
+                result.push(hex_str)
+            }
+            Err(args) => {
+                let err = format!("Deserialization error, {:?}", args);
+                println!("{}", err)
+            }
+        }
+    }
+    return result;
+}
+pub fn all_state_type_utxo() -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+    let mut utxo_storage = UTXO_STORAGE.lock().unwrap();
+    let input_type = IOType::State as usize;
+    let utxos = utxo_storage.data.get_mut(&input_type).unwrap();
+    for (key, output_data) in utxos {
+        match bincode::deserialize(&key) {
+            Ok(value) => {
+                let utxo: Utxo = value;
+                let hex_str: String = utxo.to_hex();
+                result.push(hex_str)
+            }
+            Err(args) => {
+                let err = format!("Deserialization error, {:?}", args);
+                println!("{}", err)
+            }
+        }
+    }
+    return result;
+}
 
 pub fn all_coin_type_output() -> String {
     let mut result: Vec<Output> = Vec::new();
