650fdfd717ae797fc31b540eb67c83ea0819ef47
Siddharth Parashar <20308880+shanu516516@users.noreply.github.com>
2024-05-24 21:16:44 +0000

    bug fix

--- utxo-in-memory/src/db/address_utxo_link.rs
+++ utxo-in-memory/src/db/address_utxo_link.rs
@@ -1,18 +1,24 @@
 use crate::db::*;
-
+use serde_derive::{Deserialize, Serialize};
+use std::collections::HashMap;
+use zkvm::IOType;
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct AddressUtxoIDStorage {
-    pub data: HashMap<IOType, HashMap<String, String>>,
+    pub data: HashMap<usize, HashMap<String, String>>,
 }
 impl AddressUtxoIDStorage {
     pub fn new() -> Self {
-        struct AddressUtxoIDStorage {
+        AddressUtxoIDStorage {
             data: HashMap::new(),
         }
     }
-    pub fn get_utxo_id_by_address(address: String, input_type: IOType) -> Option<String> {
+    pub fn get_utxo_id_by_address(
+        &mut self,
+        address: String,
+        input_type: IOType,
+    ) -> Option<&String> {
         self.data
-            .get_mut(&input_type)
+            .get_mut(&input_type.to_usize())
             .unwrap()
             .get(&address)
             .clone()
@@ -24,20 +30,25 @@ impl AddressUtxoIDStorage {
         utxo_id: String,
     ) -> Result<T, std::io::Error> {
         self.data
-            .get_mut(&input_type)
+            .get_mut(&input_type.to_usize())
             .unwrap()
             .insert(address.clone(), utxo_id.clone())
     }
 
     fn remove(&mut self, input_type: IOType, address: String) -> Result<T, std::io::Error> {
-        match self.data.get_mut(&input_type).unwrap().remove(&address) {
+        match self
+            .data
+            .get_mut(&input_type.to_usize())
+            .unwrap()
+            .remove(&address)
+        {
             Some(value) => {
                 return Ok(value.clone());
             }
             None => {
                 return Err(std::io::Error::new(
                     std::io::ErrorKind::NotFound,
-                    format!("utxo id:{:?} not found", id),
+                    format!("utxo id:{:?} not found", address),
                 ))
             }
         }
